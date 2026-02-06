cf448efda38d8e62c83c19c51daee2c5a18f4480
mcwachira <mcwachira@outlook.com>
2026-02-06 08:46:38 +0300

    Made some changes and updated the css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
-import Image from "next/image";
+import CTA from "@/components/home/CTA.client";
+import Features from "@/components/home/Features.client";
+import Hero from "@/components/home/Hero.client";
+import Portfolio from "@/components/home/Portfolio.client";
+import Services from "@/components/home/Service.client";
 
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Hero />
+      <Services />
+      <Portfolio />
+      <Features />
+      <CTA />
+    </>
+  );
 }
