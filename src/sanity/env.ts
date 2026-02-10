1252e71b49cbfdb6455f38412e262b094cab1506
mcwachira <mcwachira@outlook.com>
2026-02-10 21:07:55 +0300

    Setup client , env, and structure for sanity

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -11,6 +11,8 @@ export const projectId = assertValue(
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
+export const useCdn = process.env.NODE_ENV === 'production'
+
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
     throw new Error(errorMessage)
