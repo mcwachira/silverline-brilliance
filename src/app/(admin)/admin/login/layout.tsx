8d6adc56b68b1fed8fca8d1fe5aaf5e5940ad97f
mcwachira <mcwachira@outlook.com>
2026-02-19 10:38:22 +0300

    created the admin login page and admin login layout page

---
+++ src/app/(admin)/admin/login/layout.tsx
@@ -0,0 +1,11 @@
+import "../../../globals.css";
+
+export default function LoginLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="antialiased">
+        {children}
+      </body>
+    </html>
+  );
+}
