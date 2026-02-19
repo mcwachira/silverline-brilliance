cc36958e399181409cee1eb9d806294a2b55e950
mcwachira <mcwachira@outlook.com>
2026-02-19 21:28:35 +0300

    Updated the dashboard layout file

--- src/app/(admin)/admin/dashboard/layout.tsx
+++ src/app/(admin)/admin/dashboard/layout.tsx
@@ -3,18 +3,26 @@ import { createServerSupabaseClient } from '@/src/lib/supabase/server'
 import DashboardShell from '@/src/components/admin/dashboard/DashboardShell'
 import "../../../globals.css";
 
-export default async function DashboardLayout({ children , user}: { children: React.ReactNode , user?: any }) {
+export default async function DashboardLayout({ children}: { children: React.ReactNode  }) {
   const supabase = await createServerSupabaseClient()
 
-  // const { data: { user } } = await supabase.auth.getUser()
+  const { data: { user } } = await supabase.auth.getUser()
 
-  // if (!user) redirect('/admin/login')
+  if (!user) redirect('/admin/login')
+
+     // Fetch admin profile
+  const { data: profile } = await supabase
+    .from('admin_profiles')
+    .select('*')
+    .eq('id', user.id)
+    .single()
+
 
   return (
 
         <html lang="en">
       <body className="antialiased">
-    <DashboardShell user={user}>
+    <DashboardShell user={user} profile={profile}>
       {children}
     </DashboardShell>
       </body>
