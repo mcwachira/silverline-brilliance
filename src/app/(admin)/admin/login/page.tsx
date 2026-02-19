9078c36b65f7c769eb5dc22cc5ffa114ad879e29
mcwachira <mcwachira@outlook.com>
2026-02-19 22:57:54 +0300

    Updated the Login page

--- src/app/(admin)/admin/login/page.tsx
+++ src/app/(admin)/admin/login/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import LoginForm from "@/src/components/LoginForm";
+import LoginForm from "../../../../components/LoginForm";
 
 export default function LoginPage() {
   return (
